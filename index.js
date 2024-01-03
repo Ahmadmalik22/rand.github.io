e4e587252e65bf9494b65a04533a39f1da80ba71
Ahmadmalik22 <ahmimalik.2811@gmail.com>
2024-01-03 21:26:04 +0500

    random pro

---
+++ index.js
@@ -0,0 +1,13 @@
+let obj={
+    ahmad:{
+        name:"ahmad",
+        class:"bscs",
+        deprt:"cs"
+    },
+    ali:{
+        name:"ali",
+        class:"bscs",
+        deprt:"it"
+    }
+}
+console.log(obj.ali.deprt);
